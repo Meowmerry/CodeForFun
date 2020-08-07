ddf964ac8d9c1b667cdcf3c63ceb9f9cf3edf85e
Meow Merry <meow3525@gmail.com>
2020-08-07 09:37:24 +0700

    added

---
+++ Project/MagicEightBall.js
@@ -0,0 +1,42 @@
+//define a variable called userName
+let userName = 'Meow merry';
+userName ? console.log(`Hello, ${userName} `) : console.log('Hello')
+
+//Create a variable named userQuestion
+const userQuestion = 'Will I become a superman tonight?';
+console.log(`${userName} asked : ${userQuestion}`)
+
+//Create another variable, and name it randomNumber. Set it equal to this expression, which uses two methods from the Math library.
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch(randomNumber){
+  case 0:
+    eightBall ='It is certain';
+    break;
+  case 1:
+    eightBall ='It is decidedly so';
+    break;
+  case 2:
+    eightBall ='Reply hazy try again';
+    break;
+  case 3:
+    eightBall ='Cannot predict now';
+    break;
+  case 4:
+    eightBall ='Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    break;
+}
+console.log(`The answer is : ${eightBall} `)
